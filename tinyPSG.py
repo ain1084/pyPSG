eb6166929bcbda4ca34ee36596ed96be2344280d
Seiji Ainoguchi <seiji.ainoguchi@gmail.com>
2019-08-07 00:36:56 +0900

    change volume lookup table

--- tinyPSG.py
+++ tinyPSG.py
@@ -1,23 +1,10 @@
 class SampleGenerator:
     class MixingLookupTable:
         def __init__(self):
-            volres = [
-                1.0 / 15950, 1.0 / 15350, 1.0 / 15090, 1.0 / 14760,
-                1.0 / 14275, 1.0 / 13620, 1.0 / 12890, 1.0 / 11370,
-                1.0 / 10600, 1.0 / 8590, 1.0 / 7190, 1.0 / 5985,
-                1.0 / 4820, 1.0 / 3945, 1.0 / 3017, 1.0 / 2345
-            ]
-            mixRegister = 1000
-            mixRegisterUp = 800000
-            mixRegisterDown = 8000000
-
-            temp = [
-                (3.0 / mixRegisterUp + i + j + k) / (3.0 / mixRegisterUp + 3.0 / mixRegisterDown + 1.0 / mixRegister + i + j + k)
-                for k in volres for j in volres for i in volres
-            ]
-            minValue = min(10.0, min(temp))
-            maxValue = max(0.0, max(temp))
-            self.__data = [(data - minValue) / (maxValue - minValue) for data in temp]
+            # The following table generation was based on fmgen_008.lzh (Copyright (C) cisc 1997, 1999)
+            # URL: http://retropc.net/cisc/sound/
+            mul = 1 / pow(2, 1 / 4) ** 2
+            self.__data = [1.0 / 3.0 * pow(mul, i) if i != 16 else 0 for i in range(16, 0, -1)]
 
         def __getitem__(self, index):
             return self.__data[index]
@@ -28,7 +15,7 @@ class SampleGenerator:
                 self.__masterFrequency = int(masterFrequency)
                 self.__samplingFrequencyMul8 = int(samplingFrequency) << 3
                 self.__error = self.__masterFrequency
-                self.__tuneMin = self.__masterFrequency / self.__samplingFrequencyMul8 + 1
+                self.__tuneMin = int(self.__masterFrequency / self.__samplingFrequencyMul8) + 1
                 self.__source = self.__tuneMin * self.__samplingFrequencyMul8
                 self.__nextSource = self.__source
                 self.__output = False
@@ -48,9 +35,8 @@ class SampleGenerator:
                     self.__source = self.__nextSource
                 return self.__output
 
-        def __init__(self, channelNumber, masterFrequency, samplingFrequency):
-            self.__volumeIndex = 0
-            self.__volumeShift = int(channelNumber) * 4
+        def __init__(self, masterFrequency, samplingFrequency):
+            self.__volume = 0
             self.__isToneOn = True
             self.__isNoiseOn = False
             self.__toneGenerator = self.ToneGenerator(int(masterFrequency), int(samplingFrequency))
@@ -60,20 +46,20 @@ class SampleGenerator:
             self.__isNoiseOn = bool(isNoiseOn)
 
         def setVolume(self, volume):
-            self.__volumeIndex = int(volume) << self.__volumeShift
+            self.__volume = int(volume)
 
         def setTune(self, tune):
             self.__toneGenerator.setTune(int(tune))
 
         def mixWithNoise(self, isNoise):
-            return self.__volumeIndex if (self.__toneGenerator.update() & self.__isToneOn) or (isNoise & self.__isNoiseOn) else 0
+            return self.__volume if (self.__toneGenerator.update() & self.__isToneOn) or (isNoise & self.__isNoiseOn) else 0
 
     class NoiseGenerator:
         def __init__(self, masterFrequency, samplingFrequency):
             self.__masterFrequency = int(masterFrequency)
             self.__samplingFrequencyMul16 = int(samplingFrequency) << 4
             self.__error = self.__masterFrequency
-            self.__tuneMin = self.__masterFrequency / self.__samplingFrequencyMul16 + 1
+            self.__tuneMin = int(self.__masterFrequency / self.__samplingFrequencyMul16) + 1
             self.__source = self.__tuneMin * self.__samplingFrequencyMul16
             self.__nextSource = self.__source
             self.__shift = 1
@@ -97,7 +83,7 @@ class SampleGenerator:
 
     def __init__(self, masterFrequency, samplingFrequency):
         self.__noiseGenerator = self.NoiseGenerator(masterFrequency, samplingFrequency)
-        self.__channels = [self.ToneChannel(ch, masterFrequency, samplingFrequency) for ch in range(3)]
+        self.__channels = [self.ToneChannel(masterFrequency, samplingFrequency) for ch in range(3)]
         self.__mixingLookupTable = self.MixingLookupTable()
 
     def __getitem__(self, channelNumber):
@@ -108,7 +94,4 @@ class SampleGenerator:
 
     def nextSample(self):
         isNoise = self.__noiseGenerator.update()
-        index = 0
-        for channel in self.__channels:
-            index |= channel.mixWithNoise(isNoise)
-        return self.__mixingLookupTable[index]
+        return sum([self.__mixingLookupTable[channel.mixWithNoise(isNoise)] for channel in self.__channels])
